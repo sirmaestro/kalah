696464f6ea34e2dc3c3fc0325d06a3e4a5099fd3
Jordan <jche681@aucklanduni.ac.nz>
2019-05-26 16:38:01 +1200

    Vertical print

--- src/kalah/Controller/Game.java
+++ src/kalah/Controller/Game.java
@@ -22,7 +22,7 @@ public class Game {
                 break;
             }
 
-            print.game(board);
+            print.gameVertical(board);
             String input = print.getPlayerMove(player_turn);
 
             if (input.equals("q")) {
@@ -56,14 +56,14 @@ public class Game {
     public void gameOver() {
         game_over = true;
         print.gameOver();
-        print.game(board);
+        print.gameVertical(board);
     }
 
     public void gameOverWithScore() {
         game_over = true;
-        print.game(board);
+        print.gameVertical(board);
         print.gameOver();
-        print.game(board);
+        print.gameVertical(board);
         int[] scores = board.score();
         print.score(scores[0], scores[1]);
         if (scores[0] > scores[1]) {
--- src/kalah/View/KalahPrint.java
+++ src/kalah/View/KalahPrint.java
@@ -54,6 +54,54 @@ public class KalahPrint {
         io.println(braces);
     }
 
+    public void gameVertical(Board board) {
+        String player_braces_top = "+---------------+";
+        String player_braces_bottom = "+-------+-------+";
+
+        String p2_print;
+        String p1_print;
+        String houses_print = "";
+
+        if (board.getStoreValue(2) >= 10){
+            p2_print = String.format("|       | P2 %d |", board.getStoreValue(2));
+        } else {
+            p2_print = String.format("|       | P2  %d |", board.getStoreValue(2));
+        }
+
+        io.println(player_braces_top);
+        io.println(p2_print);
+        io.println(player_braces_bottom);
+
+
+
+        for (int i=0, j=board.getHouses()-1; i<board.getHouses(); i++, j--) {
+
+            if (board.getSeedValue(1,i) >= 10) {
+                houses_print = houses_print.concat(String.format("| %d[%d] |", i+1, board.getSeedValue(1,i)));
+            } else {
+                houses_print = houses_print.concat(String.format("| %d[ %d] |", i+1, board.getSeedValue(1,i)));
+            }
+
+            if (board.getSeedValue(2,j) >= 10) {
+                houses_print = houses_print.concat(String.format(" %d[%d] |", j+1, board.getSeedValue(2,j)));
+            } else {
+                houses_print = houses_print.concat(String.format(" %d[ %d] |", j+1, board.getSeedValue(2,j)));
+            }
+            io.println(houses_print);
+            houses_print = "";
+        }
+
+        if (board.getStoreValue(1) >= 10){
+            p1_print = String.format("| P1 %d |       |", board.getStoreValue(1));
+        } else {
+            p1_print = String.format("| P1  %d |       |", board.getStoreValue(1));
+        }
+
+        io.println(player_braces_bottom);
+        io.println(p1_print);
+        io.println(player_braces_top);
+    }
+
     public void score(int p1_score, int p2_score) {
         String line_1 = String.format("\tplayer 1:%d", p1_score);
         String line_2 = String.format("\tplayer 2:%d", p2_score);
